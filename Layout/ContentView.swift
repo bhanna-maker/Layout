2c3bac7a79b4265693e836cb5b79ca0f5f7e9401
Benjamine Paul Hanna <bhanna@ascensionacademy.org>
2026-01-15 09:21:35 -0600

    Initial Commit

---
+++ Layout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Layout
+//
+//  Created by Benjamine Hanna on 1/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Layout/LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutApp.swift
+//  Layout
+//
+//  Created by Benjamine Hanna on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
